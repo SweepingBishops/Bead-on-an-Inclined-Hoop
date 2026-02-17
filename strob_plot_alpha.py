8c0f263546a09e3ef3695495afda557530f65683
Roshan Mathew Philip <roshan.thakidiyel@gmail.com>
2026-02-17 20:32:33 +0530

    Larger range stroboscopic plots for the Hamiltonian case

--- strob_plot_alpha.py
+++ strob_plot_alpha.py
@@ -3,29 +3,27 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-plots_dir = "Plots/strob_plots_varying_alpha/"
+plots_dir = "Plots/strob_plots_varying_alpha/hamiltonian/"
 
-with h5py.File("Data/dissip_trajectories.h5", "r") as file:
+with h5py.File(f"Data/poincare_trajectories.h5", "r") as file:
     alphas_deg = np.arange(0,90,0.2)
     omegas = [i for i in range(1,11)]
-    #omegas = [5]
 
     for omega in omegas:
         alpha_array = list()
         theta_array = list()
         for alpha in alphas_deg:
-            grp = file[f"alpha{alpha:05.2f}/omega{omega:06.3f}/init00.0_00.0_0.1"]
+            grp = file[f"alpha{alpha:05.2f}/omega{omega:06.3f}/init00.0_00.0"]
 
             theta = grp["theta"][:]
-            t = grp["t"][:]
+            #t = grp["t"][:]
             theta = (np.array(theta) + np.pi) % (2*np.pi) - np.pi  #  Plotting theta in the range -pi to pi
 
-            dt_estimate = np.median(np.diff(t))
-            T = 2*np.pi/omega
-            nT = np.round(t / T).astype(int)
-            mask = np.abs(t - nT*T) < dt_estimate/2
-            theta = theta[mask][-50:]
-
+            # dt_estimate = np.median(np.diff(t))
+            # T = 2*np.pi/omega
+            # nT = np.round(t / T).astype(int)
+            # mask = np.abs(t - nT*T) < dt_estimate/2
+            # theta = theta[mask][-50:]
 
             alpha = [alpha]*np.size(theta)
             theta_array.extend(theta)
@@ -42,6 +40,6 @@ with h5py.File("Data/dissip_trajectories.h5", "r") as file:
                   )
         plt.grid(False)
 
-        plt.savefig(f"{plots_dir}dissip_{omega:04.1f}.jpg")
+        plt.savefig(f"{plots_dir}{omega:04.1f}.jpg")
         print(f"{plots_dir}{omega:04.1f}.jpg")
         plt.close()
--- strob_plot_omega.py
+++ strob_plot_omega.py
@@ -3,27 +3,30 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-plots_dir = "Plots/strob_plots_varying_omega/"
+plots_dir = "Plots/strob_plots_varying_omega/hamiltonian/"
 
-with h5py.File("Data/dissip_trajectories.h5", "r") as file:
-    for alpha in range(0,90, 5):
+with h5py.File("Data/poincare_trajectories.h5", "r") as file:
+    for alpha in range(0,91, 2):
         alpha_grp = file[f"alpha{alpha:05.2f}"]
         omega_array = list()
         theta_array = list()
         for omega_val in np.arange(1,11,0.1): 
             omega_grp = alpha_grp[f"omega{omega_val:06.3f}"]
             omega = omega_grp.attrs["omega"]
-            init_grp = omega_grp["init00.0_00.0_0.1"]
+            init_grp = omega_grp["init00.0_00.0"]
+            theta0 = init_grp.attrs["theta0"]
+            p0 = init_grp.attrs["p0"]
             # for init_grp in omega_grp.values():
             theta = init_grp["theta"][:]
-            t = init_grp["t"][:]
             theta = (np.array(theta) + np.pi) % (2*np.pi) - np.pi  #  Plotting theta in the range -pi to pi
-            dt_estimate = np.median(np.diff(t))
-            
-            T = 2*np.pi/omega
-            nT = np.round(t / T).astype(int)
-            mask = np.abs(t - nT*T) < dt_estimate/2
-            theta = theta[mask][:]
+
+            # t = init_grp["t"][:]
+            # dt_estimate = np.median(np.diff(t))
+            # 
+            # T = 2*np.pi/omega
+            # nT = np.round(t / T).astype(int)
+            # mask = np.abs(t - nT*T) < dt_estimate/2
+            # theta = theta[mask][:]
 
             omega = [omega]*np.size(theta)
             theta_array.extend(theta)
@@ -35,10 +38,12 @@ with h5py.File("Data/dissip_trajectories.h5", "r") as file:
         plt.ylabel("Stroboscopic sampling of " + r"$\theta(t=nT)$")
         plt.title("Stroboscopic "
                   r"$\theta$" " vs. " r"$\omega$"
-                  "\n" rf"$\alpha={alpha:05.2f}^\circ,\,gamma={init_grp.attrs['gamma']}$"
+                  #"\n" rf"$\alpha={alpha:05.2f}^\circ,\,gamma={init_grp.attrs['gamma']}$"
+                  "\n" rf"$\alpha={alpha:05.2f}^\circ$"
+                  "\n" rf"$\theta_0={np.rad2deg(theta0):.2f}^\circ,\, p_0={p0}$"
                   )
 
-        file_name = f"{plots_dir}dissip_{alpha:04.1f}.jpg"
-        plt.savefig(file_name)
+        file_name = f"{plots_dir}{alpha:05.2f}.jpg"
+        plt.savefig(file_name, bbox_inches="tight", pad_inches=0.2)
         print(file_name)
         plt.close()
